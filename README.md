a52c092c190d8537e5d50ac8a0e931ce449570a4
Marko Novak <nouvak@gmail.com>
2025-03-19 02:08:45 +0100

    Updated Conan 2 recipe so the library gets built successfully.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # conan2-SimpleAmqpClient
 A port of https://github.com/db4/conan-SimpleAmqpClient to conan2.
+
+## Setup
+
+```bash
+conan create . --build=missing
+```
--- conanfile.py
+++ conanfile.py
@@ -5,19 +5,19 @@ from conan.tools.scm import Version
 import os
 
 class SimpleAmqpClientConan(ConanFile):
-    name = "SimpleAmqpClient"
+    name = "simpleamqpclient"
     version = "2.4.0"
     license = "MIT"
     description = "SimpleAmqpClient is an easy-to-use C++ wrapper around the rabbitmq-c C library"
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
     default_options = {"shared": True, "fPIC": True}
-    requires = ["rabbitmq-c/0.6.0", "boost/[>=1.66.0]"]
+    requires = ["rabbitmq-c/0.9.0", "boost/[>=1.66.0]"]
     exports_sources = "CMakeLists.txt", "Modules/*"
 
     @property
-    def _source_subfolder(self):
-        return "source_subfolder"
+    def src_dir(self):
+        return "%s-%s" % (self.name, self.version)
 
     def config_options(self):
         if self.settings.os == "Windows":
@@ -28,11 +28,19 @@ class SimpleAmqpClientConan(ConanFile):
         if self.options.get_safe("shared"):
             self.options["boost"].fPIC = True
         self.options["rabbitmq-c"].shared= self.options.get_safe("shared", False) # add rabbitmq-c shared option
+        self.options["rabbitmq-c"].ssl = True # add rabbitmq-c ssl option
 
     def source(self):
         get(self, "https://codeload.github.com/alanxz/SimpleAmqpClient/zip/v%s" % self.version,
             sha1="931e2aa78fc011f8d1ea312541df75b1d5edd559",
-            destination=self._source_subfolder, strip_root=True)
+            destination=self.src_dir, strip_root=True)
+        cmakelist_tst = os.path.join(self.src_dir, "CMakeLists.txt")
+        os.unlink("%s/Modules/FindRabbitmqc.cmake" % self.src_dir)
+        replace_in_file(self, cmakelist_tst, "SET(CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/Modules)", "list(APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_SOURCE_DIR}/Modules)")
+        replace_in_file(self, cmakelist_tst, "${Rabbitmqc_SSL_ENABLED}", "ON")
+        replace_in_file(self, cmakelist_tst, "Rabbitmqc_LIBRARY", "rabbitmq-c_LIBRARIES")
+        replace_in_file(self, cmakelist_tst, "Rabbitmqc", "rabbitmq-c")
+        #replace_in_file(self, cmakelist_tst, "Boost", "boost")
 
     def generate(self):
         tc = CMakeToolchain(self)
@@ -46,7 +54,7 @@ class SimpleAmqpClientConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure(build_script_folder=self._source_subfolder)
+        cmake.configure(build_script_folder=self.src_dir)
         cmake.build()
         cmake.install()
 
@@ -55,8 +63,8 @@ class SimpleAmqpClientConan(ConanFile):
         copy(self, pattern="*.lib", src=os.path.join(self.package_folder, "install"), dst=os.path.join(self.package_folder, "lib"), keep_path=False)
         copy(self, pattern="*.dll", src=os.path.join(self.package_folder, "install"), dst=os.path.join(self.package_folder, "bin"), keep_path=False)
         copy(self, pattern="*.pdb", src=os.path.join(self.package_folder, "install"), dst=os.path.join(self.package_folder, "bin"), keep_path=False)
-        copy(self, pattern="*.so*", src=os.path.join(self.package_folder, "install"), dst=os.path.join(self.package_folder, "lib"), keep_path=False, symlinks=True)
-        copy(self, pattern="*.dylib", src=os.path.join(self.package_folder, "install"), dst=os.path.join(self.package_folder, "lib"), keep_path=False, symlinks=True)
+        copy(self, pattern="*.so*", src=os.path.join(self.package_folder, "install"), dst=os.path.join(self.package_folder, "lib"), keep_path=False)
+        copy(self, pattern="*.dylib", src=os.path.join(self.package_folder, "install"), dst=os.path.join(self.package_folder, "lib"), keep_path=False)
         copy(self, pattern="*.a", src=os.path.join(self.package_folder, "install"), dst=os.path.join(self.package_folder, "lib"), keep_path=False)
         # remove empty folders
         rm(self, pattern="install", folder=self.package_folder, recursive=True)
